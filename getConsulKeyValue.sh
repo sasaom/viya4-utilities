672e4a02065b27b3bb6904a19e81bf4f442749e6
sasaom <sasaom@SASOffice365.onmicrosoft.com>
2023-05-10 07:25:16 +0000

    .

--- getConsulKeyValue.sh
+++ getConsulKeyValue.sh
@@ -10,6 +10,6 @@ exitWithError() {
 CONSULPATH=$1
 [[ -z $CONSULPATH ]] && CONSULPATH='/'
 
-kubectl -n $VIYA_NAMESPACE exec -it sas-consul-server-0 -c sas-consul-server -- bash -c "export CONSUL_HTTP_ADDR=https://localhost:8500; /opt/sas/viya/home/bin/sas-bootstrap-config kv read --recurse config $1" > CONSUL-KEY-VALUE.txt
+kubectl -n $VIYA_NAMESPACE exec -it sas-consul-server-0 -c sas-consul-server -- bash -c "export CONSUL_HTTP_ADDR=https://localhost:8500; /opt/sas/viya/home/bin/sas-bootstrap-config kv read --recurse $1" > CONSUL-KEY-VALUE.txt
 
 echo "Consul Key-Value pairs has been saved to the file: CONSUL-KEY-VALUE.txt"
