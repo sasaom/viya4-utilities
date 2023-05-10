9a781abf23a2c533ca5898af6fef95ac6a09f1d3
sasaom <sasaom@SASOffice365.onmicrosoft.com>
2023-05-10 07:30:48 +0000

    .

--- getConsulKeyValue.sh
+++ getConsulKeyValue.sh
@@ -8,9 +8,9 @@ exitWithError() {
 [[ -z $VIYA_NAMESPACE ]] && exitWithError "Environment variable VIYA_NAMESPACE not defined."
 
 CONSULPATH=$1
-[[ "$CONSULPATH" == "" ]] && CONSULPATH='config'
+[[ "$CONSULPATH" == "" ]] && CONSULPATH='/'
 echo "Extracting key-values from path: $CONSULPATH"
 
-kubectl -n $VIYA_NAMESPACE exec -it sas-consul-server-0 -c sas-consul-server -- bash -c "export CONSUL_HTTP_ADDR=https://localhost:8500; /opt/sas/viya/home/bin/sas-bootstrap-config kv read --recurse $1" > CONSUL-KEY-VALUE.txt
+kubectl -n $VIYA_NAMESPACE exec -it sas-consul-server-0 -c sas-consul-server -- bash -c "export CONSUL_HTTP_ADDR=https://localhost:8500; /opt/sas/viya/home/bin/sas-bootstrap-config kv read --recurse $CONSULPATH" > CONSUL-KEY-VALUE.txt
 
 echo "Consul Key-Value pairs has been saved to the file: CONSUL-KEY-VALUE.txt"
