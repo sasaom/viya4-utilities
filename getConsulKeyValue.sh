a4920800f8947b49d3e9766c7458e025259999ba
sasaom <sasaom@SASOffice365.onmicrosoft.com>
2023-05-10 07:01:08 +0000

    First commit

---
+++ getConsulKeyValue.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+exitWithError() {
+  echo "*ERROR* $1"
+  exit 1
+}
+
+[[ -z $VIYA_NAMESPACE ]] && exitWithError "Environment variable VIYA_NAMESPACE not defined."
+
+CONSULPATH=$1
+[[ -z $CONSULPATH ]] && $CONSULPATH='/'
+
+kubectl -n $VIYA_NAMESPACE exec -it sas-consul-server-0 -c sas-consul-server -- bash -c "export CONSUL_HTTP_ADDR=https://localhost:8500; /opt/sas/viya/home/bin/sas-bootstrap-config kv read --recurse config $1"
+
