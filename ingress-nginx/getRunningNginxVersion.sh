8bb88e58bc6e37e80c31b157bbdd53a872e87f6a
Siro Bettio <siro.bettio@sas.com>
2024-09-25 08:48:40 +0000

    .

--- ingress-nginx/getRunningNginxVersion.sh
+++ ingress-nginx/getRunningNginxVersion.sh
@@ -3,6 +3,6 @@
 NS=ingress-nginx
 echo "Ingress-Nginx running version (namespace: $NS)"
 
-PODNAME=$(kubectl -n $NS get pods -l app.kubernetes.io/instance=ingress-nginx -o name)
+PODNAME=$(kubectl -n $NS get pods -l app.kubernetes.io/instance=ingress-nginx,app.kubernetes.io/component=controller -o name)
 
 kubectl exec -it -n $NS $PODNAME -- /nginx-ingress-controller --version
